1a93010b11fa12224bfa99309f8d08be3fec1416
sharavana07 <sharavanaragav@gmail.com>
2025-08-21 00:08:33 +0530

    Added india maps data as normal folders

---
+++ indianMap/mapdata.js
@@ -0,0 +1,379 @@
+var simplemaps_countrymap_mapdata={
+  main_settings: {
+   //General settings
+    width: "responsive", //'700' or 'responsive'
+    background_color: "#FFFFFF",
+    background_transparent: "yes",
+    border_color: "#ffffff",
+    
+    //State defaults
+    state_description: "State description",
+    state_color: "#88A4BC",
+    state_hover_color: "#3B729F",
+    state_url: "",
+    border_size: 1.5,
+    all_states_inactive: "no",
+    all_states_zoomable: "yes",
+    
+    //Location defaults
+    location_description: "Location description",
+    location_url: "",
+    location_color: "#FF0067",
+    location_opacity: 0.8,
+    location_hover_opacity: 1,
+    location_size: 25,
+    location_type: "square",
+    location_image_source: "frog.png",
+    location_border_color: "#FFFFFF",
+    location_border: 2,
+    location_hover_border: 2.5,
+    all_locations_inactive: "no",
+    all_locations_hidden: "no",
+    
+    //Label defaults
+    label_color: "#ffffff",
+    label_hover_color: "#ffffff",
+    label_size: 16,
+    label_font: "Arial",
+    label_display: "auto",
+    label_scale: "yes",
+    hide_labels: "no",
+    hide_eastern_labels: "no",
+   
+    //Zoom settings
+    zoom: "yes",
+    manual_zoom: "yes",
+    back_image: "no",
+    initial_back: "no",
+    initial_zoom: "-1",
+    initial_zoom_solo: "no",
+    region_opacity: 1,
+    region_hover_opacity: 0.6,
+    zoom_out_incrementally: "yes",
+    zoom_percentage: 0.99,
+    zoom_time: 0.5,
+    
+    //Popup settings
+    popup_color: "white",
+    popup_opacity: 0.9,
+    popup_shadow: 1,
+    popup_corners: 5,
+    popup_font: "12px/1.5 Verdana, Arial, Helvetica, sans-serif",
+    popup_nocss: "no",
+    
+    //Advanced settings
+    div: "map",
+    auto_load: "yes",
+    url_new_tab: "no",
+    images_directory: "default",
+    fade_time: 0.1,
+    link_text: "View Website",
+    popups: "detect",
+    state_image_url: "",
+    state_image_position: "",
+    location_image_url: ""
+  },
+  state_specific: {
+    INAN: {
+      name: "Andaman and Nicobar",
+      hover_color: "#234461"
+    },
+    INAP: {
+      name: "Andhra Pradesh",
+      hover_color: "#234461"
+    },
+    INAR: {
+      name: "Arunachal Pradesh",
+      hover_color: "#234461"
+    },
+    INAS: {
+      name: "Assam",
+      hover_color: "#234461"
+    },
+    INBR: {
+      name: "Bihar",
+      hover_color: "#234461"
+    },
+    INCH: {
+      name: "Chandigarh",
+      hover_color: "#234461"
+    },
+    INCT: {
+      name: "Chhattisgarh",
+      hover_color: "#234461"
+    },
+    INDH: {
+      name: "Dādra and Nagar Haveli and Damān and Diu",
+      hover_color: "#234461"
+    },
+    INDL: {
+      name: "Delhi",
+      hover_color: "#234461"
+    },
+    INGA: {
+      name: "Goa",
+      hover_color: "#234461"
+    },
+    INGJ: {
+      name: "Gujarat",
+      hover_color: "#234461"
+    },
+    INHP: {
+      name: "Himachal Pradesh",
+      hover_color: "#234461"
+    },
+    INHR: {
+      name: "Haryana",
+      hover_color: "#234461"
+    },
+    INJH: {
+      name: "Jharkhand",
+      hover_color: "#234461"
+    },
+    INJK: {
+      name: "Jammu and Kashmir",
+      hover_color: "#234461"
+    },
+    INKA: {
+      name: "Karnataka",
+      hover_color: "#234461"
+    },
+    INKL: {
+      name: "Kerala",
+      hover_color: "#234461"
+    },
+    INLA: {
+      name: "Ladakh",
+      hover_color: "#234461"
+    },
+    INLD: {
+      name: "Lakshadweep",
+      hover_color: "#234461"
+    },
+    INMH: {
+      name: "Maharashtra",
+      hover_color: "#234461"
+    },
+    INML: {
+      name: "Meghalaya",
+      hover_color: "#234461"
+    },
+    INMN: {
+      name: "Manipur",
+      hover_color: "#234461"
+    },
+    INMP: {
+      name: "Madhya Pradesh",
+      hover_color: "#234461"
+    },
+    INMZ: {
+      name: "Mizoram",
+      hover_color: "#234461"
+    },
+    INNL: {
+      name: "Nagaland",
+      hover_color: "#234461"
+    },
+    INOR: {
+      name: "Orissa",
+      hover_color: "#234461"
+    },
+    INPB: {
+      name: "Punjab",
+      hover_color: "#234461"
+    },
+    INPY: {
+      name: "Puducherry",
+      hover_color: "#234461"
+    },
+    INRJ: {
+      name: "Rajasthan",
+      hover_color: "#234461"
+    },
+    INSK: {
+      name: "Sikkim",
+      hover_color: "#234461"
+    },
+    INTG: {
+      name: "Telangana",
+      hover_color: "#234461"
+    },
+    INTN: {
+      name: "Tamil Nadu",
+      hover_color: "#234461"
+    },
+    INTR: {
+      name: "Tripura",
+      hover_color: "#234461"
+    },
+    INUP: {
+      name: "Uttar Pradesh",
+      hover_color: "#234461"
+    },
+    INUT: {
+      name: "Uttaranchal",
+      hover_color: "#234461"
+    },
+    INWB: {
+      name: "West Bengal",
+      hover_color: "#234461"
+    }
+  },
+  locations: {
+    "0": {
+      name: "New Delhi",
+      lat: "28.6",
+      lng: "77.2"
+    }
+  },
+  labels: {
+    INAN: {
+      name: "Andaman and Nicobar",
+      parent_id: "INAN"
+    },
+    INAP: {
+      name: "Andhra Pradesh",
+      parent_id: "INAP"
+    },
+    INAR: {
+      name: "Arunachal Pradesh",
+      parent_id: "INAR"
+    },
+    INAS: {
+      name: "Assam",
+      parent_id: "INAS"
+    },
+    INBR: {
+      name: "Bihar",
+      parent_id: "INBR"
+    },
+    INCH: {
+      name: "Chandigarh",
+      parent_id: "INCH"
+    },
+    INCT: {
+      name: "Chhattisgarh",
+      parent_id: "INCT"
+    },
+    INDH: {
+      name: "Dādra and Nagar Haveli and Damān and Diu",
+      parent_id: "INDH"
+    },
+    INDL: {
+      name: "Delhi",
+      parent_id: "INDL"
+    },
+    INGA: {
+      name: "Goa",
+      parent_id: "INGA"
+    },
+    INGJ: {
+      name: "Gujarat",
+      parent_id: "INGJ"
+    },
+    INHP: {
+      name: "Himachal Pradesh",
+      parent_id: "INHP"
+    },
+    INHR: {
+      name: "Haryana",
+      parent_id: "INHR"
+    },
+    INJH: {
+      name: "Jharkhand",
+      parent_id: "INJH"
+    },
+    INJK: {
+      name: "Jammu and Kashmir",
+      parent_id: "INJK"
+    },
+    INKA: {
+      name: "Karnataka",
+      parent_id: "INKA"
+    },
+    INKL: {
+      name: "Kerala",
+      parent_id: "INKL"
+    },
+    INLA: {
+      name: "Ladakh",
+      parent_id: "INLA"
+    },
+    INLD: {
+      name: "Lakshadweep",
+      parent_id: "INLD"
+    },
+    INMH: {
+      name: "Maharashtra",
+      parent_id: "INMH"
+    },
+    INML: {
+      name: "Meghalaya",
+      parent_id: "INML"
+    },
+    INMN: {
+      name: "Manipur",
+      parent_id: "INMN"
+    },
+    INMP: {
+      name: "Madhya Pradesh",
+      parent_id: "INMP"
+    },
+    INMZ: {
+      name: "Mizoram",
+      parent_id: "INMZ"
+    },
+    INNL: {
+      name: "Nagaland",
+      parent_id: "INNL"
+    },
+    INOR: {
+      name: "Orissa",
+      parent_id: "INOR"
+    },
+    INPB: {
+      name: "Punjab",
+      parent_id: "INPB"
+    },
+    INPY: {
+      name: "Puducherry",
+      parent_id: "INPY"
+    },
+    INRJ: {
+      name: "Rajasthan",
+      parent_id: "INRJ"
+    },
+    INSK: {
+      name: "Sikkim",
+      parent_id: "INSK"
+    },
+    INTG: {
+      name: "Telangana",
+      parent_id: "INTG"
+    },
+    INTN: {
+      name: "Tamil Nadu",
+      parent_id: "INTN"
+    },
+    INTR: {
+      name: "Tripura",
+      parent_id: "INTR"
+    },
+    INUP: {
+      name: "Uttar Pradesh",
+      parent_id: "INUP"
+    },
+    INUT: {
+      name: "Uttaranchal",
+      parent_id: "INUT"
+    },
+    INWB: {
+      name: "West Bengal",
+      parent_id: "INWB"
+    }
+  },
+  legend: {
+    entries: []
+  },
+  regions: {}
+};
